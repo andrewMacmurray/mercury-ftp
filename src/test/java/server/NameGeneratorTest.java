602c4d9cfd416be50f779a869e6bc86988b8ad79
andrewMacmurray <a.macmurray@icloud.com>
2018-06-08 13:39:00 +0100

    clean up flow of nameGenerator test

--- src/test/java/server/NameGeneratorTest.java
+++ src/test/java/server/NameGeneratorTest.java
@@ -1,17 +1,25 @@
 package server;
 
+import org.junit.Before;
 import org.junit.Test;
 import server.ftpcommands.utils.NameGenerator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class NameGeneratorTest {
 
-    private List<String> existingNames = Arrays.asList("hello.txt", "hello-1.txt", "hello-2.txt", "hello.world.txt");
+    private List<String> existingNames;
     private NameGenerator nameGenerator = new NameGenerator(this::fileExists);
+
+    @Before
+    public void setup() {
+        existingNames = new ArrayList<>();
+    }
 
     @Test
     public void generate() {
@@ -21,12 +29,18 @@ public class NameGeneratorTest {
 
     @Test
     public void generateUnique() {
+        existingNames.add("hello.txt");
+        existingNames.add("hello-1.txt");
+        existingNames.add("hello-2.txt");
+
         String name = nameGenerator.generateUnique("hello.txt");
         assertEquals("hello-3.txt", name);
     }
 
     @Test
     public void dottedUnique() {
+        existingNames.add("hello.world.txt");
+        
         String name = nameGenerator.generateUnique("hello.world.txt");
         assertEquals("hello.world-1.txt", name);
     }
