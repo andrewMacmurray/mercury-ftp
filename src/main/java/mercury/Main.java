1b0346923882d085a409113580bd3127398aa119
andrewMacmurray <a.macmurray@icloud.com>
2018-06-28 17:32:14 +0100

    re add params from command line args

--- src/main/java/mercury/Main.java
+++ src/main/java/mercury/Main.java
@@ -12,20 +12,17 @@ import java.util.concurrent.Executors;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        String publicIp = args[0];
-//        String userRootDirectory = args[1];
-//
-//        if (publicIp == null || userRootDirectory == null) {
-//            throw new RuntimeException("Please pass public_ip and user_root_directory as arguments");
-//        }
+        String publicIp = args[0];
+        String userRootDirectory = args[1];
 
-        String userRootDirectory = "ftp";
-        String publicIp = "127.0.0.1";
+        if (publicIp == null || userRootDirectory == null) {
+            throw new RuntimeException("Please pass public_ip and user_root_directory as arguments");
+        }
 
         Integer fromPort = 2022;
         Integer toPort = 2026;
 
-        ServerSocket serverSocket = new ServerSocket(2021);
+        ServerSocket serverSocket = new ServerSocket(21);
         NativeFileSystem fs = new NativeFileSystem(userRootDirectory);
         PassivePortManager passivePortManager = new PassivePortManager(publicIp, fromPort, toPort);
         SocketFactory socketFactory = new SocketFactory();
