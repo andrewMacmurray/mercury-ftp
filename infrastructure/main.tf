4401596576b84c5b6b0216a1527d07aa52c0b64c
andrewMacmurray <a.macmurray@icloud.com>
2018-06-28 22:13:02 +0100

    removes template file for boot sequence

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,15 +4,11 @@ resource "aws_instance" "mercury" {
   key_name = "mercury-aws-key"
   iam_instance_profile = "${aws_iam_instance_profile.ec2_s3_access.name}"
   vpc_security_group_ids = ["${aws_security_group.mercury-security.id}"]
-  user_data = "${data.template_file.boot_sequence.rendered}"
+  user_data = "${file("boot-sequence.sh")}"
 
   tags {
     Name = "mercury-terraform"
   }
-}
-
-data "template_file" "boot_sequence" {
-  template = "${file("./boot-sequence.sh")}"
 }
 
 output "public_ip" {
